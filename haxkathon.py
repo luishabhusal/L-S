af12fd9859eaa1b3106f18e8c35358cd4e775b80
luishabhusal <luisabhusal11@gmail.com>
2026-07-05 08:24:50 +0545

    Added restaurant Script

---
+++ haxkathon.py
@@ -0,0 +1,18 @@
+
+menu = """
+Food:
+- Aglio e Olio : Rs. 850
+- Spicy Wings : Rs. 850
+- Pepperoni Pizza : Rs. 1250
+- Periperi Chicken : Rs. 1780
+- L&S Special Chicken Biryani : Rs. 950
+
+Drinks:
+- Coke : Rs. 520
+- Mojito : Rs. 600
+- Margarita : Rs. 700
+- Pina Colada : Rs. 800
+"""
+print("Welcome to L&S Lounge!")
+print("Type 'exit' to quit.\n")
+
